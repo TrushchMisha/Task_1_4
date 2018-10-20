3f88230075116827980c2e877253db8e4a55387e
TrushchMisha <43968047+TrushchMisha@users.noreply.github.com>
2018-10-20 21:39:20 +0300

    Homework

---
+++ src/main/java/com/tm/Main.java
@@ -0,0 +1,18 @@
+package com.tm;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Day: ");
+        String a = in.nextLine();
+        System.out.println("Time(hours): ");
+        String b = in.nextLine();
+        int n = Integer.parseInt(b);
+        if((a.equals("Friday")) || (a.equals("Monday"))) {
+            if((n <= 20) && (n >= 18)) System.out.println("Lesson is running. You are late! Hurry up!");
+            else System.out.println("Lesson is not running.");
+        }
+        else System.out.println("Lesson is not running.");
+    }
+}
